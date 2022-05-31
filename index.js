a0501bdfcae6e327a7b4a3e1f9bf8b7db39014e1
Martin Ondejka <martin.ondejka@vacuumlabs.com>
2022-05-31 13:16:45 +0200

    Add response to the assessment.start

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const uuid = require("uuid");
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
@@ -29,7 +31,27 @@ app.post("/assessment.list", (req, res) => {
 
 app.post("/assessment.start", (req, res) => {
   console.log(req.body);
-  res.json({ success: false });
+
+  const assessment_id = uuid.v4();
+  console.log(`New Assessment ID: ${assessment_id}`);
+
+  res.json({
+    success: true,
+    results: {
+      assessment_id,
+      _comment: "some comment",
+      update_request: {
+        timestamp: new Date().getTime(),
+        assessment_id,
+        assessment_status: {
+          identifier: "assignment_status",
+          value: "not_started",
+          type: "string",
+          label: "Assignment status",
+        },
+      },
+    },
+  });
 });
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
