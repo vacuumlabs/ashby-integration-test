e296d5bbe1a2edf78f08c979051baa3f0a66d7c7
Martin Ondejka <martin.ondejka@vacuumlabs.com>
2022-05-31 13:00:35 +0200

    Add body log do the assessment start

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT ?? 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+  res.send("Hello world");
+});
 
 app.post("/assessment.list", (req, res) => {
-    res.json({
-        success: true,
-        results: [
-            {
-                assessment_type_id: "1",
-                name: "synonyms",
-                description: "proste synonyma"
-            },
-            {
-                assessment_type_id: "2",
-                name: "dependencies",
-                description: "default assignment"
-            }
-        ]
-})
-})
+  res.json({
+    success: true,
+    results: [
+      {
+        assessment_type_id: "1",
+        name: "synonyms",
+        description: "proste synonyma",
+      },
+      {
+        assessment_type_id: "2",
+        name: "dependencies",
+        description: "default assignment",
+      },
+    ],
+  });
+});
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}`))
+app.post("/assessment.start", (req, res) => {
+  console.log(req.body);
+  res.json({ success: false });
+});
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
